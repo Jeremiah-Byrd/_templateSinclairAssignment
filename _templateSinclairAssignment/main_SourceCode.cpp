2279a4ec48ca223c72e303ea495ef8ef94abe973
Jeremiah-Byrd <101752111+Jeremiah-Byrd@users.noreply.github.com>
2022-04-26 22:18:35 -0400

    Added the driver

---
+++ _templateSinclairAssignment/main_SourceCode.cpp
@@ -0,0 +1,22 @@
+// Jeremiah Byrd 
+// CIS 1202 101
+// April 26, 2022
+#include<iostream>
+#include<iomanip>
+#include<template>
+using namespace std;
+
+//Prototypes 
+
+int main() {
+	//Variables 
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	//Testing the template class
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+}
